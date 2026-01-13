b14b3af26088c18abf3b22fc76b972d9daed2cb9
unknown <muhualis@gmail.com>
2026-01-13 17:20:01 +0300

    Complete Task 2: Chunking, Embedding, and Vector Store Indexing

--- src/vector_store_manager.py
+++ src/vector_store_manager.py
@@ -8,7 +8,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
-
+ 
+ 
 class VectorStoreManager:
     def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
         """
